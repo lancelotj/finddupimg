2154a216512c2f1f4f0e0b41e4b28dd242a4bde0
Lance Jian <lancelotj100@gmail.com>
2016-05-23 15:12:05 -0700

    Adds send to trash function

--- dupproc.py
+++ dupproc.py
@@ -4,7 +4,37 @@
 import sys
 import json
 import argparse
-from finddup import print_err
+from finddupimg import print_err
+from send2trash import send2trash
+
+
+def filter_dict(hash_dict):
+    for key, group in hash_dict.items():
+        if len(group) > 1:
+            yield group
+
+
+def fmt_report(hash_dict, print_out):
+    dup_count = 0
+    for group in filter_dict(hash_dict):
+        dup_count += len(group) - 1
+    print_err('Total %s images. %s duplictes' % (len(hash_dict), dup_count))
+
+
+def fmt_html(hash_dict, print_out):
+    print_out('<html><body>\n')
+    for group in filter_dict(hash_dict):
+        print_out('<hr/>')
+        for info in group:
+            print_out('<div><a href="%(path)s" target="_blank"><img width="150px" src="file://%(path)s" /></a> Path: %(path)s, Size: %(size)s, Image Size: %(image_size)s</div>' % info)
+    print_out('</body></html>')
+
+
+def fmt_trash(hash_dict, print_out):
+    for group in filter_dict(hash_dict):
+        for info in group[1:]:
+            print_out('Send %s to trash...' % info['path'])
+            send2trash(info['path'])
 
 
 def main(args):
@@ -13,19 +43,12 @@ def main(args):
     def print_out(content):
         print(content, file=args.output)
 
-    print_out('<html><body>\n')
+    {
+        'report': fmt_report,
+        'html': fmt_html,
+        'trash': fmt_trash,
+    }[args.format](hash_dict, print_out)
 
-    dup_count = 0
-    for key, group in hash_dict.items():
-        count = len(group)
-        if count > 1:
-            dup_count += count
-            print_out('<hr/>')
-            for info in group:
-                print_out('<div><img width="150px" src="file://%(path)s" /> Path: %(path)s Size: %(size)s</div>' % info)
-
-    print_out('</body></html>')
-    print_err('Total %s images. %s duplictes' % (len(hash_dict), dup_count))
 
 if __name__ == '__main__':
 
@@ -38,7 +61,7 @@ if __name__ == '__main__':
         '-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
         help='output')
     parser.add_argument(
-        '-f', '--format', choice=('html', 'report'), default='report',
+        '-f', '--format', choices=('html', 'report', 'trash'), default='report',
         help='format')
 
     main(parser.parse_args())
