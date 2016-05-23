77efdf8063fe152e49febc1266b86c0b0b6eb291
Lance Jian <lancelotj100@gmail.com>
2016-05-23 14:00:50 -0700

    Adds dup2html, finishes finddup

---
+++ dup2html.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import argparse
+
+def errprint(content):
+    print(content, file=sys.stderr)
+
+
+def main(args):
+    hash_dict = json.loads(args.input.read())
+
+    def oprint(content):
+        print(content, file=args.output)
+
+    oprint('<html><body>\n')
+
+    dup_count = 0
+    for key, group in hash_dict.items():
+        count = len(group)
+        if count > 1:
+            dup_count += count
+            oprint('<hr/>')
+            for info in group:
+                oprint('<div><img width="150px" src="file://%(path)s" /> Path: %(path)s Size: %(size)s</div>' % info)
+
+    oprint('</body></html>')
+    errprint('Total %s images. %s duplictes' % (len(hash_dict), dup_count))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=('Looking up a director to see if there are duplicated file'))
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin, help='input')
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help='output')
+
+    main(parser.parse_args())
--- finddup.py
+++ finddup.py
@@ -2,50 +2,92 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import json
+import imagehash
+import argparse
+import collections
+from operator import itemgetter
+
 from collections import defaultdict
-from hashlib import md5
-from argparse import ArgumentParser
+from PIL import Image, ExifTags
 
 
-def get_md5(dup):
-    with open(dup) as f:
-        m = md5()
-        #f.seek(-1000, os.SEEK_END)
-        m.update(f.read())
-        return m.digest()
+def printerr(content):
+    print(content, file=sys.stderr)
 
+
+def get_file_size(file_name):
+    return os.path.getsize(file_name)
+
+
+def get_image_size(img):
+    return "{} x {}".format(*img.size)
+
+
+def is_image(thefile):
+    fname, ext = os.path.splitext(thefile)
+    return not fname.startswith('.') and ext.lower() in set(['.jpg', '.jpeg', '.gif', '.png', '.tiff'])
+
+def walk_images(path):
+    for root, dirs, files in os.walk(path):
+        dirs[:] = filter(lambda d: not d.startswith('.'), dirs)
+        for fname in files:
+            if is_image(fname):
+                path = os.path.join(root, fname)
+                with Image.open(path) as img:
+                    dup_info = {
+                        'hash': str(imagehash.phash(img)),
+                        'path': path,
+                        'size': get_file_size(path),
+                        'image_size': get_image_size(img),
+                    }
+                    yield dup_info
+
+
+
+def main(args):
+    dup_count = 0
+    total = 0
+    if args.existing:
+        src_dict = defaultdict(list, json.load(args.existing))
+    else:
+        src_dict = defaultdict(list)
+
+    try:
+        for path in args.dirs:
+            for dup_info in walk_images(path):
+                total += 1
+                printerr('Processing %s.' % dup_info['path'])
+                src_dict[dup_info['hash']].append({
+                    'path': dup_info['path'],
+                    'size': dup_info['size'],
+                    'image_size': dup_info['image_size'],
+                })
+    except KeyboardInterrupt:
+        pass
+    printerr('\n%d files processed.' % total)
+    output = collections.OrderedDict((
+        info[0], sorted(info[1], key=itemgetter('size'), reverse=True)
+    ) for info in sorted(
+        src_dict.items(),
+        key=lambda d: len(d[1])))
+
+    for hash_str, dups in output.items():
+        count = len(dups)
+        if count > 1:
+            dup_count += count
+
+    print(json.dumps(output, indent=2), file=args.output)
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description=('Looking up a director to see if there are duplicated file'))
+    parser = argparse.ArgumentParser(
+        description=('Looking up a director to see if there are duplicated file'))
     parser.add_argument('dirs', nargs='+', help='Target directories')
-    parser.add_argument('-v', '--verbose', action='count', help='More information')
+    parser.add_argument(
+        '-v', '--verbose', action='store_false', help='More information')
+    parser.add_argument(
+        '-e', '--existing', type=argparse.FileType('r'),
+        default=None, help='Use this as existing hash table.')
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help='output')
 
-    args = parser.parse_args()
-    src_dict = defaultdict(list)
-    for path in args.dirs:
-        for root, dirs, files in os.walk(path):
-            for f in files:
-                fext = os.path.splitext(f)[1].lower()
-                if fext in set(('.jpg', '.jpeg', '.nef', '.mov', '.png', '.gif')):
-                    fname = os.path.join(root, f)
-                    size = os.path.getsize(fname)
-                    src_dict[size].append(fname)
-    if args.verbose:
-        sys.stderr.write('%d files processed\n' % len(src_dict))
-
-    hashes = defaultdict(list)
-    for size, dups in src_dict.iteritems():
-        if len(dups) > 1:
-            if args.verbose:
-                print '\t'.join(dups)
-            for dup in dups:
-                hashes[get_md5(dup)].append(dup)
-
-    count = 0
-    for code, dups in hashes.iteritems():
-        if len(dups) > 1:
-            count += 1
-            print '\t'.join('"%s"' % dup for dup in sorted(dups))
-
-    if args.verbose:
-        sys.stderr.write('%d duplicated files found.\n' % count)
+    main(parser.parse_args())
